d8bd43a98ddbdee0a8fb83e391e4a4e5588a2565
Ryan Solida <ryan@shoutitlouder.com>
2015-08-08 23:11:06 -0400

    update perms

--- lumen/recipes/set_permissions.rb
+++ lumen/recipes/set_permissions.rb
@@ -1,10 +1,10 @@
 node[:deploy].each do |application, deploy|
-    script "set_permissions" do
-      interpreter "bash"
-      user "root"
-      cwd "#{deploy[:deploy_to]}/current"
-      code <<-EOH
-      chmod -R 777 storage
-      EOH
-    end
+  script "set_permissions" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    chmod -R 777 storage
+    EOH
+  end
 end
