6d178f68485833fd8164b2a5d26b072adea8034b
Ryan Solida <ryan@shoutitlouder.com>
2015-08-08 22:51:39 -0400

    syntax

--- lumen/recipes/set_permissions.rb
+++ lumen/recipes/set_permissions.rb
@@ -1,10 +1,12 @@
-node[:deploy].each do |app_name, deploy|
- if deploy[:application] == "platform"
- script "set_permissions" do
-  interpreter "bash"
-  user "root"
-  cwd "#{deploy[:deploy_to]}/current/app"
-  code <<-EOH
-  chmod -R 777 storage
-  EOH
+node[:deploy].each do |application, deploy|
+  if deploy[:application] == "platform"
+    script "set_permissions" do
+      interpreter "bash"
+      user "root"
+      cwd "#{deploy[:deploy_to]}/current/app"
+      code <<-EOH
+      chmod -R 777 storage
+      EOH
+    end
+  end
 end
