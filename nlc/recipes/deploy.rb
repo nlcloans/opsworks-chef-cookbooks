75e490d6c6b1a77882d15c11e052fb7c6f389305
Ryan Solida <ryan@shoutitlouder.com>
2015-08-21 14:39:58 -0400

    Testing config steps

--- nlc/recipes/deploy.rb
+++ nlc/recipes/deploy.rb
@@ -1,14 +1,13 @@
 node[:deploy].each do |application, deploy|
-  script "set_permissions" do
-    interpreter "bash"
-    user "root"
-    cwd "#{deploy[:deploy_to]}/current"
-    code <<-EOH
-    touch storage/logs/lumen.log
-    chmod -R 777 ./storage
-    chmod -R 777 ./public/docs
-    EOH
-  end
+
+  interpreter "bash"
+  user "root"
+  cwd "#{deploy[:deploy_to]}/current"
+  code <<-EOH
+  touch storage/logs/lumen.log
+  chmod -R 777 ./storage
+  chmod -R 777 ./public/docs
+  EOH
 
   # write out .env file
   template "#{deploy[:deploy_to]}/current/.env" do
