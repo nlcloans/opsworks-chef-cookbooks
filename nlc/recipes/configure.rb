d69255ed3e4dfe08d75c60fb9a2fe00f6564fff7
Ryan Solida <ryan@shoutitlouder.com>
2015-08-21 14:49:21 -0400

    Added scripting for supervisor

---
+++ nlc/recipes/configure.rb
@@ -0,0 +1,30 @@
+node[:deploy].each do |application, deploy|
+
+  # INSTALL AND ADD QUEUE TO SUPERVISOR
+  script "download_supervisor" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    apt-get install supervisor
+    EOH
+  end
+
+  # write out .env file
+  template "/etc/supervisor/conf.d/queue.conf" do
+    source 'supervisor.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+  end
+
+  script "run_supervisor" do
+    interpreter "bash"
+    user "root"
+    code <<-EOH
+    supervisorctl add queue
+    EOH
+  end
+  #END SUPERVISOR INSTALL
+
+end
--- nlc/recipes/deploy.rb
+++ nlc/recipes/deploy.rb
@@ -1,13 +1,15 @@
 node[:deploy].each do |application, deploy|
-
-  interpreter "bash"
-  user "root"
-  cwd "#{deploy[:deploy_to]}/current"
-  code <<-EOH
-  touch storage/logs/lumen.log
-  chmod -R 777 ./storage
-  chmod -R 777 ./public/docs
-  EOH
+  
+  script "set_permissions" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    touch storage/logs/lumen.log
+    chmod -R 777 ./storage
+    chmod -R 777 ./public/docs
+    EOH
+  end
 
   # write out .env file
   template "#{deploy[:deploy_to]}/current/.env" do
