1589d9c20b23d995f882722014a3d3ae6c843c0c
Ryan Solida <ryan@shoutitlouder.com>
2015-08-21 14:31:43 -0400

    testing new structure

--- nlc/metadata.rb
+++ nlc/metadata.rb
@@ -1,6 +1,6 @@
-name              "lumen"
+name              "nlc"
 maintainer        "Ryan Solida"
 maintainer_email  "ryan@mozay.com"
 license           "MIT"
-description       "Sets up necessary steps for running an lumen application"
+description       "Sets up necessary steps for running the NLC process"
 version           "1.0.0"
